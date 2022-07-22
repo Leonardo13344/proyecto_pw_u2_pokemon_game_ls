b2c1d2cbb7166f7b1f857b51184f003b7f9ee136
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2022-07-21 19:49:05 -0500

    taller18

---
+++ src/components/PokemonOptions.vue
@@ -0,0 +1,39 @@
+<template>
+  <!--TODO OPtions-->
+  <div class="options-container">
+    <ul>
+      <li>growlithe</li>
+      <li>swarmpet</li>
+      <li>3</li>
+      <li>4</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+ul {
+  list-style-type: none;
+}
+
+li {
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  width: 200px;
+  margin-bottom: 10px;
+  cursor: pointer;
+}
+
+li:hover {
+  background-color: rgba(0, 0, 0.5);
+}
+.options-container{
+    justify-content: center;
+    display: flex;
+}
+
+</style>
---
+++ src/components/PokemonPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <!--TODO Page-->
+  <PokemonPicture :pokemonId="478" :mostrarPokemon="true"/>
+  <PokemonOptions />
+</template>
+
+<script>
+
+import PokemonOptions from './PokemonOptions'
+import PokemonPicture from './PokemonPicture'
+export default {
+components:{
+    PokemonPicture,
+    PokemonOptions
+}
+}
+</script>
+
+<style>
+
+</style>
